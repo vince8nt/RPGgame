e1a4a42f67657182c95b19c37c5a9dc27d9a7900
vtittert <vtittert@ucsc.edu>
2020-09-01 14:08:52 -0700

    first commit

---
+++ js/game.js
@@ -0,0 +1,133 @@
+var c = document.getElementById("gameContainer");
+var ctx = c.getContext("2d");
+ctx.webkitImageSmoothingEnabled = false;
+ctx.mozImageSmoothingEnabled = false;
+ctx.imageSmoothingEnabled = false;
+
+class Tileset {
+	constructor(context, imageSrc, width, height, tileSize) {
+		this.context = context;
+		this.tileset = new Image();
+		this.tileset.src = imageSrc;
+		this.width = width;
+		this.height = height;
+		this.tileSize = tileSize;
+	}
+	drawTile(tileNum, x, y, size) {
+		var tileX = tileNum % this.width;
+		var tileY = Math.floor(tileNum / this.width);
+		var tileSize = this.tileSize;
+		var imgX = tileX * tileSize;
+		var imgY = tileY * tileSize;
+		this.context.drawImage(this.tileset, imgX, imgY, tileSize, tileSize, x, y, size, size);
+	}
+	drawChunk(chunk, x, y, size) {
+
+	}
+}
+
+class ChunkMap {
+	constructor() {
+		this.chunks = [];
+	}
+	loadChunk(fileSrc) {
+		var req = new XMLHttpRequest();
+		req.onload = function(){
+		    process_webgl_data(this.responseText);
+		};
+		req.open('GET', fileSrc);
+		req.send();
+	}
+}
+
+var keysDown = [];
+for (var i = 0; i < 256; i++)
+	keysDown[i] = false;
+document.addEventListener("keydown", function(event) {
+	keysDown[event.keyCode] = true;
+});
+document.addEventListener("keyup", function(event) {
+	keysDown[event.keyCode] = false;
+});
+
+
+// -------------------------------------------------------------------------------------------
+
+myChunkMap = new ChunkMap();
+myChunkMap.loadChunk("js/chunks/test");
+
+myTileset = new Tileset(ctx, "images/tileset.png", 16, 16, 16);
+setTimeout(moveLoop, 100, 0, 0);
+
+function testDraw(x, y) {
+	ctx.clearRect(0, 0, c.width, c.height);
+	for (var j = 0; j < 16; j++) {
+		for (var i = 0; i < 16; i++)
+			myTileset.drawTile(j * 16 + i, x + i * 32, y + j * 32, 32);
+	}
+}
+
+function moveLoop(x, y) {
+	var d = new Date();
+	if (keysDown[37] || keysDown[65]) {          // left arrow
+		moveX(y, x, x - 32, d.getTime(), 100);
+	}
+	else if (keysDown[38] || keysDown[87]) {     // up arrow
+		moveY(x, y, y - 32, d.getTime(), 100);
+	}
+	else if (keysDown[39] || keysDown[68]) {     // right arrow
+		moveX(y, x, x + 32, d.getTime(), 100);
+	}
+	else if (keysDown[40] || keysDown[83]) {     // down arrow
+		moveY(x, y, y + 32, d.getTime(), 100);
+	}
+	else {
+		setTimeout(moveLoop, 0, x, y);
+	}
+}
+
+function moveX(y, xBegin, xEnd, beginTime, moveTime) {
+	var d = new Date();
+	moveProgress = (d.getTime() - beginTime) / moveTime;
+	if (moveProgress >= 1) { // finish move
+		testDraw(xEnd, y);
+		setTimeout(moveLoop, 0, xEnd, y);
+	}
+	else { // move part way
+		var x = xBegin + (xEnd - xBegin) * moveProgress;
+		testDraw(x, y);
+		setTimeout(moveX, 0, y, xBegin, xEnd, beginTime, moveTime);
+	}
+}
+
+function moveY(x, yBegin, yEnd, beginTime, moveTime) {
+	var d = new Date();
+	moveProgress = (d.getTime() - beginTime) / moveTime;
+	if (moveProgress >= 1) { // finish move
+		testDraw(x, yEnd);
+		setTimeout(moveLoop, 0, x, yEnd);
+	}
+	else { // move part way
+		var y = yBegin + (yEnd - yBegin) * moveProgress;
+		testDraw(x, y);
+		setTimeout(moveY, 0, x, yBegin, yEnd, beginTime, moveTime);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
